9ad41de744df3c0c35b32589db2789d0bbce1fbc
Hien Le <lnthien@tma.com.vn>
2019-05-09 08:20:18 +0700

    fix issue cors

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var api_router_1 = require("./routes/api_router");
 var user_router_1 = require("./routes/user_router");
 var mongoose_1 = require("mongoose");
 var env_1 = require("./config/env");
+var cors = require("cors");
 var ServerExpress = /** @class */ (function () {
     function ServerExpress() {
         var _this = this;
         this.config = function () {
             _this.app.use(bodyParser.json());
             _this.app.use(bodyParser.urlencoded({ extended: false }));
+            _this.app.use(cors());
         };
         this.mongoSetup = function () {
             mongoose_1.connect("mongodb+srv://" + env_1.config.database.userName + ":" + env_1.config.database.password + "@" + env_1.config.database.databaseName +
